2d12227623e9a249dcbbf1c36a4c92b734b6da25
René Klatt <rene.klatt@spryker.com>
2019-04-30 04:42:14 +0000

    TE-1211 Refactored out Shop Yves Router from Spryker, refactored Shared Router into Spryker Router, refactored Shared RouterExtension, added WebProfilerExtension, refactored WebProfiler, added Config WebProfilerExtension

--- src/Spryker/Zed/RouterExtension/Dependency/Plugin/RouterPluginInterface.php
+++ src/Spryker/Zed/RouterExtension/Dependency/Plugin/RouterPluginInterface.php
@@ -5,7 +5,7 @@
  * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
  */
 
-namespace Spryker\Shared\RouterExtension\Dependency\Plugin;
+namespace Spryker\Zed\RouterExtension\Dependency\Plugin;
 
 use Symfony\Component\Routing\RouterInterface;
 
